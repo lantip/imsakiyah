40756e1101c4dad3e2f1ab9e4652d66dbe90205d
Muhammad Zamroni <halo@matriphe.com>
2021-04-12 22:59:54 +0200

    Add setWaktu function

--- imsakiyah.py
+++ imsakiyah.py
@@ -14,6 +14,12 @@ from ics import Calendar, Event
 import argparse
 from argparse import RawTextHelpFormatter
 
+def setWaktu(hourStr, todayTimeStr, hourDelta=7, formatTime='%Y-%m-%d %H:%M:%S'):
+    wkt = datetime.strptime(todayTimeStr+' '+hourStr+':00', formatTime)
+    wkt = wkt - timedelta(hours=hourDelta)
+
+    return wkt
+
 def generate(lat, lon, startdate, days):
     PT = PrayTimes('Makkah')
     PT.adjust({'imsak':'10 min', 'fajr':17.8, 'dhuhr':'2 min', 'asr':'1.03', 'maghrib':1.5,'isha':18.7})
